fc96921404d82833b346a51caedd2091988261b2
twa127 <46624596+twa127@users.noreply.github.com>
2021-01-09 20:24:43 +0000

    Update chart_batteryusage.php

--- chart_batteryusage.php
+++ chart_batteryusage.php
@@ -1,17 +1,15 @@
 <?php
 /*
-   _____    _   _    _
-  |  __ \  (_) | |  | |
-  | |__) |  _  | |__| |   ___    _ __ ___     ___
-  |  ___/  | | |  __  |  / _ \  | |_  \_ \   / _ \
-  | |      | | | |  | | | (_) | | | | | | | |  __/
-  |_|      |_| |_|  |_|  \___/  |_| |_| |_|  \___|
-
-     S M A R T   H E A T I N G   C O N T R O L
-
+             __  __                             _
+            |  \/  |                    /\     (_)
+            | \  / |   __ _  __  __    /  \     _   _ __
+            | |\/| |  / _` | \ \/ /   / /\ \   | | |  __|
+            | |  | | | (_| |  >  <   / ____ \  | | | |
+            |_|  |_|  \__,_| /_/\_\ /_/    \_\ |_| |_|
+                    S M A R T   T H E R M O S T A T
 *************************************************************************"
-* PiHome is Raspberry Pi based Central Heating Control systems. It runs *"
-* from web interface and it comes with ABSOLUTELY NO WARRANTY, to the   *"
+* MaxAir is a Linux based Central Heating Control systems. It runs from *"
+* a web interface and it comes with ABSOLUTELY NO WARRANTY, to the      *"
 * extent permitted by applicable law. I take no responsibility for any  *"
 * loss or damage to you or your property.                               *"
 * DO NOT MAKE ANY CHANGES TO YOUR HEATING SYSTEM UNTILL UNLESS YOU KNOW *"
@@ -35,13 +33,14 @@ var bat_level_dataset = [
     $count = mysqli_num_rows($resulta) + 1;
     while ($row = mysqli_fetch_assoc($resulta)) {
         //grab the node id to be displayed in the plot legend
-		$node_id=$row['node_id'];
-		$query="select * from zone_view where sensors_id = '{$node_id}' limit 1;";
-		$resultz = $conn->query($query);
-		$zone_row = mysqli_fetch_array($resultz);
-		$zone_name = $zone_row['name'];
-		$label = $zone_name ." - ID ".$node_id;
-    $graph_id = $node_id.".0"; //assume battery node colour same as child_id = 0
+                $id=$row['id'];
+                $node_id=$row['node_id'];
+                $query="select * from temperature_sensors where sensor_id = '{$id}' limit 1;";
+                $result_ts = $conn->query($query);
+                $temp_sensor_row = mysqli_fetch_array($result_ts);
+                $name = $temp_sensor_row['name'];
+                $label = $name ." - ID ".$id;
+                $graph_id = $id.".0"; //assume battery node colour same as child_id = 0
 		$query="SELECT bat_voltage, bat_level, `update`  FROM nodes_battery WHERE `update` >= last_day(now()) + interval 1 day - interval 3 MONTH AND bat_level is not NULL and node_id = '{$node_id}' GROUP BY Week(`update`), Day(`update`) ORDER BY `update` ASC;";
         	$result = $conn->query($query);
         	// create array of pairs of x and y values for every zone
