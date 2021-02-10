e1d6bb43c7def6f4583c02d6811168feffc31ced
twa127 <46624596+twa127@users.noreply.github.com>
2021-02-10 23:30:28 +0000

    Update settingslist.php

--- settingslist.php
+++ settingslist.php
@@ -1,17 +1,15 @@
 <?php 
 /*
-   _____    _   _    _                             
-  |  __ \  (_) | |  | |                            
-  | |__) |  _  | |__| |   ___    _ __ ___     ___  
-  |  ___/  | | |  __  |  / _ \  | |_  \_ \   / _ \ 
-  | |      | | | |  | | | (_) | | | | | | | |  __/ 
-  |_|      |_| |_|  |_|  \___/  |_| |_| |_|  \___| 
-
-     S M A R T   H E A T I N G   C O N T R O L 
-
+             __  __                             _
+            |  \/  |                    /\     (_)
+            | \  / |   __ _  __  __    /  \     _   _ __
+            | |\/| |  / _` | \ \/ /   / /\ \   | | |  __|
+            | |  | | | (_| |  >  <   / ____ \  | | | |
+            |_|  |_|  \__,_| /_/\_\ /_/    \_\ |_| |_|
+                   S M A R T   T H E R M O S T A T
 *************************************************************************"
-* PiHome is Raspberry Pi based Central Heating Control systems. It runs *"
-* from web interface and it comes with ABSOLUTELY NO WARRANTY, to the   *"
+* MaxAir is a Linux based Central Heating Control systems. It runs from *"
+* a web interface and it comes with ABSOLUTELY NO WARRANTY, to the      *"
 * extent permitted by applicable law. I take no responsibility for any  *"
 * loss or damage to you or your property.                               *"
 * DO NOT MAKE ANY CHANGES TO YOUR HEATING SYSTEM UNTILL UNLESS YOU KNOW *"
@@ -80,8 +78,8 @@ $("#ajaxModal").on("show.bs.modal", function(e) {
                                                         <h3 class="status"></small></h3>
                                                         </button>
 
-	        		                        <button class="btn btn-default btn-circle btn-xxl mainbtn animated fadeIn" data-href="edit_gpio.php" data-toggle="modal" data-target="#cron_jobs">
-	        			                <h3 class="buttontop"><small><?php echo $lang['cron_jobs']; ?></small></h3>
+							<button class="btn btn-default btn-circle btn-xxl mainbtn animated fadeIn" data-href="edit_gpio.php" data-toggle="modal" data-target="#jobs_schedule">
+                                                        <h3 class="buttontop"><small><?php echo $lang['jobs']; ?></small></h3>
 	        	        			<h3 class="degre" ><i class="ionicons ion-ios-timer-outline blue"></i></h3>
         	        	        		<h3 class="status"></small></h3>
 	                	        	        </button>
