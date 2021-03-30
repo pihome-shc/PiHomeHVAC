6b3898ab9553a34a5eed684941c085dc564dad72
twa127 <46624596+twa127@users.noreply.github.com>
2021-03-30 16:59:11 +0100

    Update getTemperature.php

--- api/getTemperature.php
+++ api/getTemperature.php
@@ -1,17 +1,15 @@
 <?php
 /*
-   _____    _   _    _
-  |  __ \  (_) | |  | |
-  | |__) |  _  | |__| |   ___    _ __ ___     ___
-  |  ___/  | | |  __  |  / _ \  | |_  \_ \   / _ \
-  | |      | | | |  | | | (_) | | | | | | | |  __/
-  |_|      |_| |_|  |_|  \___/  |_| |_| |_|  \___|
-
-     S M A R T   H E A T I N G   C O N T R O L
-
+             __  __                             _
+            |  \/  |                    /\     (_)
+            | \  / |   __ _  __  __    /  \     _   _ __
+            | |\/| |  / _` | \ \/ /   / /\ \   | | |  __|
+            | |  | | | (_| |  >  <   / ____ \  | | | |
+            |_|  |_|  \__,_| /_/\_\ /_/    \_\ |_| |_|
+                    S M A R T   T H E R M O S T A T
 *************************************************************************"
-* PiHome is Raspberry Pi based Central Heating Control systems. It runs *"
-* from web interface and it comes with ABSOLUTELY NO WARRANTY, to the   *"
+* MaxAir is a Linux based Central Heating Control systems. It runs from *"
+* a web interface and it comes with ABSOLUTELY NO WARRANTY, to the      *"
 * extent permitted by applicable law. I take no responsibility for any  *"
 * loss or damage to you or your property.                               *"
 * DO NOT MAKE ANY CHANGES TO YOUR HEATING SYSTEM UNTILL UNLESS YOU KNOW *"
@@ -28,31 +26,39 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 require_once(__DIR__.'../../st_inc/connection.php');
 require_once(__DIR__.'../../st_inc/functions.php');
 
-if(isset($_GET['zonename'])) {
-        $zonename = $_GET['zonename'];
-        $query = "SELECT * FROM zone_view where name = '{$zonename}' LIMIT 1;";
+if(isset($_GET['sensorname'])) {
+        $sensorname = $_GET['sensorname'];
+        $query = "SELECT sensor_id, sensor_child_id FROM temperature_sensors where name = '{$sensorname}' LIMIT 1;";
         $results = $conn->query($query);
         $row = mysqli_fetch_assoc($results);
         if(! $row) {
                 http_response_code(400);
-                echo json_encode(array("success" => False, "state" => "No Zone with that name found."));
+                echo json_encode(array("success" => False, "state" => "No Sensor with that name found."));
         } else {
-	        $zone_sensor_id=$row['sensors_id'];
-        	$zone_sensor_child_id=$row['sensor_child_id'];
+                $query = "SELECT node_id FROM nodes where id = '{$row['sensor_id']}' LIMIT 1;";
+                $nresult = $conn->query($query);
+                $nrow = mysqli_fetch_assoc($nresult);
+                if(! $nrow) {
+                        http_response_code(400);
+                        echo json_encode(array("success" => False, "state" => "No Node found."));
+                } else {
+                        $node_id=$nrow['node_id'];
+                        $child_id=$row['sensor_child_id'];
 
-	        //query to get temperature from messages_in_view_24h table view
-        	$query = "SELECT * FROM messages_in_view_24h WHERE node_id = '{$zone_sensor_id}' AND child_id = {$zone_sensor_child_id} ORDER BY datetime desc LIMIT 1;";
-	        $result = $conn->query($query);
-	        $sensor = mysqli_fetch_array($result);
-        	if(! $sensor) {
-                	http_response_code(400);
-                	echo json_encode(array("success" => False, "state" => "Sensor has not reported in the last 24 hours."));
-        	} else {
-		        $zone_c = $sensor['payload'];
-        		http_response_code(200);
-        		echo json_encode(array("success" => True, "state" => $zone_c));
-		}
-	}
+                        //query to get temperature from messages_in_view_24h table view
+                        $query = "SELECT * FROM messages_in_view_24h WHERE node_id = '{$node_id}' AND child_id = {$child_id} ORDER BY datetime desc LIMIT 1;";
+                        $result = $conn->query($query);
+                        $sensor = mysqli_fetch_array($result);
+                        if(! $sensor) {
+                                http_response_code(400);
+                                echo json_encode(array("success" => False, "state" => "Sensor has not reported in the last 24 hours."));
+                        } else {
+                                $zone_c = $sensor['payload'];
+                                http_response_code(200);
+                                echo json_encode(array("success" => True, "state" => $zone_c));
+                        }
+                }
+        }
 } else {
         http_response_code(400);
         echo json_encode(array("success" => False, "state" => "Data is incomplete."));
