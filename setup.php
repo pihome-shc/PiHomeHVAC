0972d7f06b012f76fe0070777aad7adf1bce5dd3
twa127 <46624596+twa127@users.noreply.github.com>
2021-04-09 08:35:51 +0100

    Update setup.php

--- setup.php
+++ setup.php
@@ -263,7 +263,7 @@ $query_job_scheduling .= "VALUES ('controller','/var/www/cron/controller.php',0,
 $query_job_scheduling .= "('check_gw','/var/www/cron/check_gw.php',0,0,60,''),";
 $query_job_scheduling .= "('system_c','/var/www/cron/system_c.php',0,0,300,''),";
 $query_job_scheduling .= "('weather_update','/var/www/cron/weather_update.php',0,0,1800,''),";
-$query_job_scheduling .= "('reboot_wifi','/var/www/cron/reboot_wifi.sh',0,0,120,'');";
+$query_job_scheduling .= "('reboot_wifi','/var/www/cron/reboot_wifi.sh',0,0,120,''),";
 $query_job_scheduling .= "('sw_install','/var/www/cron/sw_install.py',0,0,10,'');";
 $results = $conn->query($query_job_scheduling);
 if ($results) {
