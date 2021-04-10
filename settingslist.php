76caf2bf4e7ed7ffd6d6493a2235f1da824972df
twa127 <46624596+twa127@users.noreply.github.com>
2021-04-10 14:33:17 +0100

    Update settingslist.php

--- settingslist.php
+++ settingslist.php
@@ -250,7 +250,7 @@ $("#ajaxModal").on("show.bs.modal", function(e) {
         			        	        <h3 class="status"></small></h3>
 	        	        		       	</button>
 
-							<button class="btn btn-default btn-circle btn-xxl mainbtn animated fadeIn" data-toggle="modal" data-remote="false" data-target="#ajaxModal" data-ajax="ajax.php?Ajax=GetModal_Software_Install">
+							<button type="button" class="btn btn-default btn-circle btn-xxl mainbtn animated fadeIn" data-href="#" data-toggle="modal" data-target="#sw_install">
                                                         <h3 class="buttontop"><small><?php echo $lang['install_sw']; ?></small></h3>
                                                         <h3 class="degre" ><i class="fa fa-terminal"></i></h3>
                                                         <h3 class="status"></small></h3>
