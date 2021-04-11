186fb60a9ef9af57d51ebb31b78d3848e2987557
twa127 <46624596+twa127@users.noreply.github.com>
2021-04-11 19:35:45 +0100

    Update pdf_download.php

--- pdf_download.php
+++ pdf_download.php
@@ -1,4 +1,23 @@
 <?php
+/*
+             __  __                             _
+            |  \/  |                    /\     (_)
+            | \  / |   __ _  __  __    /  \     _   _ __
+            | |\/| |  / _` | \ \/ /   / /\ \   | | |  __|
+            | |  | | | (_| |  >  <   / ____ \  | | | |
+            |_|  |_|  \__,_| /_/\_\ /_/    \_\ |_| |_|
+                    S M A R T   T H E R M O S T A T
+*************************************************************************"
+* MaxAir is a Linux based Central Heating Control systems. It runs from *"
+* a web interface and it comes with ABSOLUTELY NO WARRANTY, to the      *"
+* extent permitted by applicable law. I take no responsibility for any  *"
+* loss or damage to you or your property.                               *"
+* DO NOT MAKE ANY CHANGES TO YOUR HEATING SYSTEM UNTILL UNLESS YOU KNOW *"
+* WHAT YOU ARE DOING                                                    *"
+*************************************************************************"
+*/
+
+// This script is used to open a PDF file in a new browser tab
 
 // The location of the PDF file
 // on the server
