ce4f6eeb551ec89dba50f31a8e686b2d09e27e51
twa127 <46624596+twa127@users.noreply.github.com>
2021-04-11 18:09:00 +0100

    Update pdf_download.php

--- pdf_download.php
+++ pdf_download.php
@@ -5,11 +5,15 @@
 $file = basename($_GET['file']);
 $filename = '/var/www/documentation/pdf_format/'.$file;
 
-// Header content type
-header("Content-type: application/pdf");
-
-header("Content-Length: " . filesize($filename));
-
-// Send the file to the browser.
-readfile($filename);
+$pdf = file_get_contents($filename);
+header('Content-Type: application/pdf');
+header('Cache-Control: public, must-revalidate, max-age=0'); // HTTP/1.1
+header('Pragma: public');
+header('Expires: Sat, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+header('Content-Length: '.strlen($pdf));
+header('Content-Disposition: inline; filename="'.basename($PDFfilename).'";');
+ob_clean();
+flush();
+echo $pdf;
 ?>
