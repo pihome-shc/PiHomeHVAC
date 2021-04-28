c9b0c430080760c09d3c61ff054c197eb175c30a
twa127 <46624596+twa127@users.noreply.github.com>
2021-04-28 07:49:54 +0100

    Update settingslist.php

--- settingslist.php
+++ settingslist.php
@@ -328,6 +328,12 @@ $("#ajaxModal").on("show.bs.modal", function(e) {
                                                         <h3 class="status"></small></h3>
                                                         </button>
 
+                                                	<button class="btn btn-default btn-circle btn-xxl mainbtn animated fadeIn" data-href="#" data-toggle="modal" data-target="#sensor_types">
+                                                        <h3 class="buttontop"><small><?php echo $lang['sensor_type']; ?></small></h3>
+                                                        <h3 class="degre" ><i class="fa fa-list-ol red"></i></h3>
+                                                        <h3 class="status"></small></h3>
+                                                        </button>
+
 				                       	<button class="btn btn-default btn-circle btn-xxl mainbtn animated fadeIn" data-href="#" data-toggle="modal" data-target="#sensor_gateway">
         	        			        <h3 class="buttontop"><small><?php echo $lang['gateway']; ?></small></h3>
 	        	        	                <h3 class="degre" ><i class="fa fa-heartbeat red"></i></h3>
