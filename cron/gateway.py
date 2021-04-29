8a0aaabd845419115e8240403d0cdc63a29f7cb2
twa127 <46624596+twa127@users.noreply.github.com>
2021-04-29 07:41:33 +0100

    Update gateway.py

--- cron/gateway.py
+++ cron/gateway.py
@@ -222,6 +222,11 @@ try:
             )
 
     while 1:
+        ## Terminate gateway script if no route to network gateway
+        if gatewaytype == "wifi":
+            gateway_up  = True if os.system("ping -c 1 " + gatewaylocation) is 0 else False
+            if not gateway_up:
+                break
         ## Outgoing messages
         con.commit()
         cur.execute(
