35f1ea10b5935777916b457ea52f7d5d160a4684
twa127 <46624596+twa127@users.noreply.github.com>
2021-05-05 10:27:14 +0100

    Update functions.php

--- st_inc/functions.php
+++ st_inc/functions.php
@@ -644,6 +644,7 @@ DELETE FROM controller_zone_logs WHERE `purge`= 1;
 DELETE FROM add_on_zone_logs WHERE `purge`= 1;
 DELETE FROM zone_sensors WHERE `purge`= 1;
 DELETE FROM zone_controllers WHERE `purge`= 1;
+DELETE FROM livetemp WHERE `purge`= 1 LIMIT 1;
 DELETE FROM zone WHERE `purge`= 1 LIMIT 1;
 DELETE FROM schedule_daily_time_zone WHERE `purge`= 1;
 DELETE FROM holidays WHERE `purge`= 1;
