c34c551d7f129c0cc9be9925a153e9f439fcfde9
twa127 <46624596+twa127@users.noreply.github.com>
2021-07-28 11:08:39 +0100

    Update gateway.py

--- cron/gateway.py
+++ cron/gateway.py
@@ -227,7 +227,7 @@ try:
         ## Terminate gateway script if no route to network gateway
         if gatewaytype == "wifi":
             ping_count = ping_count + 1
-            if ping_count == 360 :
+            if ping_count == 3600 :
                 ping_count = 0
                 gateway_up  = True if os.system("ping -c 1 " + gatewaylocation) is 0 else False
                 if not gateway_up:
