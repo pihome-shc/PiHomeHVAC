d03675228e89b29165e2b45598cb57f62bcd3b71
twa127 <46624596+twa127@users.noreply.github.com>
2021-08-04 13:03:04 +0100

    Update move_files.php

--- move_files.php
+++ move_files.php
@@ -10,7 +10,21 @@ function moveFolderFiles($dir){
         return;
 
     foreach($ffs as $ff){
+        // Move everything in the directory except the placeholder file
         if (strcmp($ff, 'updates.txt') !== 0) {
+                // Check if db_config.ini has been updated and if so update the version and build values in the 'system' table
+                if (strcmp($ff, 'db_config.ini') === 0) {
+                        $settings = parse_ini_file($dir.'/db_config.ini');
+                        foreach ($settings as $key => $setting) {
+                                // Notice the double $$, this tells php to create a variable with the same name as key
+                                $$key = $setting;
+                        }
+                        $conn = new mysqli($hostname, $dbusername, $dbpassword);
+                        $db_selected = mysqli_select_db($conn, $dbname);
+                        $query = "UPDATE `system` SET `version`='".$version."',`build`='".$build."';";
+                        $conn->query($query);
+                }
+                // Move updated files propper locations
                 $cmd = 'mv '.$dir.'/'.$ff.' /var/www';
                 exec($cmd);
         }
