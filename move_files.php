2d90fb6034cfd62268e9f73c9b5df6c41588e5f9
twa127 <46624596+twa127@users.noreply.github.com>
2021-08-06 16:16:40 +0100

    Update move_files.php

--- move_files.php
+++ move_files.php
@@ -1,4 +1,24 @@
 <?php
+/*
+             __  __                             _
+            |  \/  |                    /\     (_)
+            | \  / |   __ _  __  __    /  \     _   _ __
+            | |\/| |  / _` | \ \/ /   / /\ \   | | |  __|
+            | |  | | | (_| |  >  <   / ____ \  | | | |
+            |_|  |_|  \__,_| /_/\_\ /_/    \_\ |_| |_|
+                    S M A R T   T H E R M O S T A T
+*************************************************************************"
+* MaxAir is a Linux based Central Heating Control systems. It runs from *"
+* a web interface and it comes with ABSOLUTELY NO WARRANTY, to the      *"
+* extent permitted by applicable law. I take no responsibility for any  *"
+* loss or damage to you or your property.                               *"
+* DO NOT MAKE ANY CHANGES TO YOUR HEATING SYSTEM UNTILL UNLESS YOU KNOW *"
+* WHAT YOU ARE DOING                                                    *"
+*************************************************************************"
+*/
+
+// This script is used to move files in the code_updates directory to their propper locations
+
 function moveFolderFiles($dir){
     $ffs = scandir($dir);
 
