6cb73a904050660a8821e449d63ee72117f8250b
JSa1987 <62815008+JSa1987@users.noreply.github.com>
2021-09-20 16:22:32 +0100

    MQTT Nodes
    Implementation of MQTT nodes

---
+++ MySQL_Database/database_updates/200921.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS `mqtt_node_child` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `child_id` tinyint(4) NOT NULL,
+  `node_id` tinyint(4) NOT NULL,
+  `type` tinyint(4) NOT NULL COMMENT '0 - Sensor, 1 - Relay',
+  `purge` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'Mark For Deletion',
+  `name` char(50) COLLATE utf8_bin DEFAULT NULL,
+  `mqtt_topic` CHAR(50) NOT NULL COLLATE utf8_bin DEFAULT NULL COMMENT 'Relay payload for on command',
+  `on_payload` char(50) COLLATE utf8_bin DEFAULT NULL COMMENT 'Relay payload for on command',
+  `off_payload` char(50) COLLATE utf8_bin DEFAULT NULL COMMENT 'Relay payload for on command',
+  `attribute` char(50) COLLATE utf8_bin DEFAULT NULL COMMENT 'Sensor JSON attribute',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
--- cron/controller.php
+++ cron/controller.php
@@ -1597,9 +1597,9 @@ for ($row = 0; $row < count($zone_commands); $row++){
 			}
 
 			/***************************************************************************************
-			Zone Valve Wireless Section: MySensors Wireless Relay module for your Zone Valve control.
+			Zone Valve Wireless Section: MySensors Wireless or MQTT Relay module for your Zone Valve control.
 			****************************************************************************************/
-			if ($zone_controller_type == 'MySensor'){
+			if ($zone_controller_type == 'MySensor' || $zone_controller_type == 'MQTT'){
 				//update messages_out table with sent status to 0 and payload to as zone status.
 				$query = "UPDATE messages_out SET sent = '0', payload = '{$zone_command}' WHERE node_id ='$zone_controler_id' AND child_id = '$zone_controler_child_id' LIMIT 1;";
 				$conn->query($query);
@@ -1694,22 +1694,22 @@ if (in_array("1", $system_controller)) {
 		}
 
 		/**************************************************************************************************
-		System Controller Wirelss Section:	MySensors Wireless Relay module for your System Controller
+		System Controller Wirelss Section:	MySensors Wireless or MQTT Relay module for your System Controller
 		***************************************************************************************************/
 		//update messages_out table with sent status to 0 and payload to as system controller status.
                 if ($sc_mode != 3) { //process if NOT  HVAC fan only mode
-	        	if ($system_controller_mode == 1 && $off_relay_type == 'MySensor'){
+	        	if ($system_controller_mode == 1 && ($off_relay_type == 'MySensor' || $off_relay_type == 'MQTT')){
         	        	$query = "UPDATE messages_out SET sent = '0', payload = '0' WHERE node_id ='{$off_relay_id}' AND child_id = '{$off_relay_child_id}' LIMIT 1;";
 	        	        $conn->query($query);
         	        	echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - System Controller Node ID: \033[41m".$off_relay_id."\033[0m Child ID: \033[41m".$off_relay_child_id."\033[0m \n";
 		        }
-			if ($on_relay_type == 'MySensor'){
+			if ($on_relay_type == 'MySensor' || $on_relay_type == 'MQTT'){
 				$query = "UPDATE messages_out SET sent = '0', payload = '{$new_system_controller_status}' WHERE node_id ='{$on_relay_id}' AND child_id = '{$on_relay_child_id}' LIMIT 1;";
 				$conn->query($query);
 				echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - System Controller Node ID: \033[41m".$on_relay_id."\033[0m Child ID: \033[41m".$on_relay_child_id."\033[0m \n";
 			}
 		}
-        	if ($system_controller_mode == 1 && $fan_relay_type == 'MySensor'){
+        	if ($system_controller_mode == 1 && ($fan_relay_type == 'MySensor' || $fan_relay_type == 'MQTT')){
         		$query = "UPDATE messages_out SET sent = '0', payload = '{$new_system_controller_status}' WHERE node_id ='{$fan_relay_id}' AND child_id = '{$fan_relay_child_id}' LIMIT 1;";
 	                $conn->query($query);
         	        echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - System Controller Node ID: \033[41m".$fan_relay_id."\033[0m Child ID: \033[41m".$fan_relay_child_id."\033[0m \n";
@@ -1829,9 +1829,9 @@ if (in_array("1", $system_controller)) {
 		$conn->query($query);
 
 		/****************************************************************************************************
-		System Controller Wirelss Section:	MySensors Wireless Relay module for your System Controller
+		System Controller Wirelss Section:	MySensors Wireless or MQTT Relay module for your System Controller
 		*****************************************************************************************************/
-		if ($heat_relay_type == 'MySensor'){
+		if ($heat_relay_type == 'MySensor' || $heat_relay_type == 'MQTT'){
 			//update messages_out table with sent status to 0 and payload to as system controller status.
 			$query = "UPDATE messages_out SET sent = '0', payload = '{$new_system_controller_status}' WHERE node_id ='{$heat_relay_id}' AND child_id = '{$heat_relay_child_id}' LIMIT 1;";
 			$conn->query($query);
@@ -1839,11 +1839,11 @@ if (in_array("1", $system_controller)) {
 		}
         	if ($system_controller_mode == 1){
                 	//update messages_out table with sent status to 0 and payload to as system controller status.
-			if ($cool_relay_type == 'MySensor') { // HVAC cool relay OFF
+			if ($cool_relay_type == 'MySensor' || $cool_relay_type == 'MQTT') { // HVAC cool relay OFF
         	                $query = "UPDATE messages_out SET sent = '0', payload = '0' WHERE node_id ='{$cool_relay_id}' AND child_id = '{$cool_relay_child_id}' LIMIT 1;";
 				$conn->query($query);
 			}
-	                if ($fan_relay_type == 'MySensor') {
+	                if ($fan_relay_type == 'MySensor' || $fan_relay_type == 'MQTT') {
 				if ($sc_mode == 3) { // HVAC fan ON if set to fan mode, else turn OFF
 	        	                $query = "UPDATE messages_out SET sent = '0', payload = '1' WHERE node_id ='{$fan_relay_id}' AND child_id = '{$fan_relay_child_id}' LIMIT 1;";
 				} else {
