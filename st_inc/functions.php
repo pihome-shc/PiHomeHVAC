9e80f2e99afd3995c7bfa4020ec797a4c0262e6b
twa127 <46624596+twa127@users.noreply.github.com>
2021-11-01 20:22:35 +0000

    Update functions.php

--- st_inc/functions.php
+++ st_inc/functions.php
@@ -741,53 +741,6 @@ function scan_db_update_dir($dir) {
         return ($files) ? $files : false;
 }
 
-// command line spinner function
-function show_status($done, $total, $size=30) {
-
-    static $start_time;
-
-    // if we go over our bound, just ignore it
-    if($done > $total) return;
-
-    if(empty($start_time)) $start_time=time();
-    $now = time();
-
-    $perc=(double)($done/$total);
-
-    $bar=floor($perc*$size);
-
-    $status_bar="\r[";
-    $status_bar.=str_repeat("=", $bar);
-    if($bar<$size){
-        $status_bar.=">";
-        $status_bar.=str_repeat(" ", $size-$bar);
-    } else {
-        $status_bar.="=";
-    }
-
-    $disp=number_format($perc*100, 0);
-
-    $status_bar.="] $disp%  $done/$total";
-
-    $rate = ($now-$start_time)/$done;
-    $left = $total - $done;
-    $eta = round($rate * $left, 2);
-
-    $elapsed = $now - $start_time;
-
-    $status_bar.= " remaining: ".number_format($eta)." sec.  elapsed: ".number_format($elapsed)." sec.";
-
-    echo "$status_bar  ";
-
-    flush();
-
-    // when done, send a newline
-    if($done == $total) {
-        echo "\n";
-    }
-
-}
-
 // get the schedule status by zone_id, start/stop times can be sunrise/sunset dependant on flag setting
 function get_schedule_status($conn,$zone_id,$holidays_status){
         // get current day number
