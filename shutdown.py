71a7b9a4faa1c7b838375cb6884ef3312898d96f
twa127 <46624596+twa127@users.noreply.github.com>
2021-11-04 15:12:43 +0000

    Update shutdown.py

--- shutdown.py
+++ shutdown.py
@@ -2,4 +2,4 @@ command = "/usr/bin/sudo /sbin/shutdown -h now"
 import subprocess
 process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
 output = process.communicate()[0]
-print output
+print(output)
