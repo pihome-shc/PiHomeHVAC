5319394be10f1d4a64ea10f8992718d17d255926
twa127 <46624596+twa127@users.noreply.github.com>
2021-11-09 19:16:23 +0000

    Update model.php

--- model.php
+++ model.php
@@ -1364,6 +1364,14 @@ echo '
             		<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
                			<h5 class="modal-title">'.$lang['smart_home_gateway'].'</h5>
+                                <div class="dropdown pull-right">
+                                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                                                <i class="fa fa-file fa-fw"></i><i class="fa fa-caret-down"></i>
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                		<li><a href="pdf_download.php?file=setup_guide_gateway.pdf" target="_blank"><i class="fa fa-file fa-fw"></i>'.$lang['setup_guide_gateway'].'</a></li>
+                                        </ul>
+                                </div>
             		</div>
             		<div class="modal-body">';
 				$gquery = "SELECT * FROM gateway";
