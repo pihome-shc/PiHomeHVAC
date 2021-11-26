fde21cda1d443bc4ed6601df5a731d09798a0ef1
twa127 <46624596+twa127@users.noreply.github.com>
2021-11-26 11:03:36 +0000

    Update 261121.sql

--- MySQL_Database/database_updates/261121.sql
+++ MySQL_Database/database_updates/261121.sql
@@ -3,4 +3,4 @@ SELECT * FROM (SELECT '0' AS `sync`, '0' AS `purge`, 'HVAC-M' AS `type`, '4' AS 
 WHERE NOT EXISTS (
     SELECT `type` FROM `zone_type` WHERE `type` = 'HVAC-M'
 ) LIMIT 1;
-ALTER TABLE `system_controller` ADD `hvac_relays` TINYINT(4) NOT NULL AFTER `fan_relay_id`;
+ALTER TABLE `system_controller` ADD COLUMN IF NOT EXISTS `hvac_relays_state` TINYINT(4) NOT NULL AFTER `fan_relay_id`;
