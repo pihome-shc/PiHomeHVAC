6048b4d56537e54368442f3cfddd5c00de38b185
twa127 <46624596+twa127@users.noreply.github.com>
2022-01-09 18:18:12 +0000

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -430,6 +430,8 @@ function GetModal_MQTT($conn)
                 </a>
                 <ul class="dropdown-menu">
                         <li><a href="pdf_download.php?file=setup_guide_mqtt.pdf" target="_blank"><i class="fa fa-file fa-fw"></i>'.$lang['setup_guide_mqtt'].'</a></li>
+                        <li class="divider"></li>
+                        <li><a href="pdf_download.php?file=setup_zigbee2mqtt.pdf" target="_blank"><i class="fa fa-file fa-fw"></i>'.$lang['setup_zigbee2mqtt'].'</a></li>
                 </ul>
              </div>
         </div>
