c1bf583224349d3f55f58840bef405f5da94632b
twa127 <46624596+twa127@users.noreply.github.com>
2022-01-23 15:28:18 +0000

    Update lv.php

--- languages/lv.php
+++ languages/lv.php
@@ -820,4 +820,6 @@ $lang['sensor_limits_add_error'] = "Senor Limits addition failed with error: ";
 $lang['sensor_limits_modify_success'] = "Senor Limits Modified Successfully.";
 $lang['weather_factoring'] = "Weather Factoring";
 $lang['weather_factoring_info'] = "Apply a weather temperature related factor to the zone control sensor reading.";
+$lang['weather_sensor'] = "Weather Sensor";
+$lang['weather_sensor_info'] = "Sensor Name to be used for Weather Factoring";
 ?>
