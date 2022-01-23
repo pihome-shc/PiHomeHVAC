b715899241f7112542aa0e5edf0b0ee853f3b19a
twa127 <46624596+twa127@users.noreply.github.com>
2022-01-23 08:58:55 +0000

    Update 230122.sql

--- MySQL_Database/database_updates/230122.sql
+++ MySQL_Database/database_updates/230122.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `system_controller` ADD COLUMN IF NOT EXISTS `weather_factoring` TINYINT(1) NOT NULL AFTER `hvac_relays_state`;
-UPDATE `system_controller` SET `weather_factoring`= 0;
+UPDATE `system_controller` SET `weather_factoring`= 1;
