818c7d33f355addf3f1bb9d16662ff896a375c5f
twa127 <46624596+twa127@users.noreply.github.com>
2022-02-01 13:40:43 +0000

    Update install.sh

--- add_on/HomeAssistant/install.sh
+++ add_on/HomeAssistant/install.sh
@@ -6,10 +6,10 @@
 MODEL=$(tr -d '\0' </proc/device-tree/model)
 if [[ $MODEL == *"Raspberry"* ]]; then
   echo "Model: Raspberry"
-  REQUIREMENTS=requirements_RPi.txt
+  REQUIREMENTS=/var/www/add_on/HomeAssistant/requirements_RPi.txt
 else
   echo "Model: generic SBC"
-  REQUIREMENTS=requirements.txt
+  REQUIREMENTS=/var/www/add_on/HomeAssistant/requirements.txt
 fi
 
 PYTHON3DEV=$( apt-cache pkgnames python3-dev )
@@ -17,15 +17,15 @@ if [[ $PYTHON3DEV == *"python3-dev"* ]]; then
   echo "Python3-dev is already installed"
 else
   echo "Installing Python3-dev"
-  apt-get install -y python3-dev
+  sudo apt-get install -y python3-dev
 fi
 
 echo "Installing Phyton modules"
-pip3 install -r $REQUIREMENTS
+sudo pip3 install -r $REQUIREMENTS
 
 echo "Creating service for auto start"
-cp HA_integration.service /etc/systemd/system/HA_integration.service
-systemctl enable HA_integration.service
+sudo cp /var/www/add_on/HomeAssistant/HA_integration.service /etc/systemd/system/HA_integration.service
+sudo systemctl enable HA_integration.service
 
 echo "Starting the service"
-systemctl start HA_integration.service
+sudo systemctl start HA_integration.service
