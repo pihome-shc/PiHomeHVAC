9671c669ff1ac00f59675dbe9dc4a68ac76a85aa
twa127 <46624596+twa127@users.noreply.github.com>
2022-02-18 10:12:26 +0000

    Update homelist.php

--- homelist.php
+++ homelist.php
@@ -416,7 +416,14 @@ if(settings($conn, 'language') == "sk" || settings($conn, 'language') == "de") {
 										echo '<p>Coop Start Schedule - Waiting for System Controller start.</p>';
 									}
 								}
-								$squery = "SELECT * FROM schedule_daily_time_zone_view where zone_id ='{$zone_id}' AND tz_status = 1 AND time_status = '1' AND (WeekDays & (1 << {$dow})) > 0 AND type = 0 ORDER BY start asc";
+//								$squery = "SELECT * FROM schedule_daily_time_zone_view where zone_id ='{$zone_id}' AND tz_status = 1 AND time_status = '1' AND (WeekDays & (1 << {$dow})) > 0 AND type = 0 ORDER BY start asc";
+                                                                $squery = "SELECT schedule_daily_time.sch_name, schedule_daily_time.start, schedule_daily_time.end,
+                                                                schedule_daily_time_zone.temperature, schedule_daily_time_zone.id AS tz_id
+                                                                FROM `schedule_daily_time`, `schedule_daily_time_zone`
+                                                                WHERE (schedule_daily_time.id = schedule_daily_time_zone.schedule_daily_time_id) AND schedule_daily_time.status = 1
+                                                                AND schedule_daily_time_zone.status = 1 AND schedule_daily_time.type = 0 AND schedule_daily_time_zone.zone_id ='{$zone_id}'
+                                                                AND (schedule_daily_time.WeekDays & (1 << {$dow})) > 0
+                                                                ORDER BY schedule_daily_time.start asc;";
 								$sresults = $conn->query($squery);
 								if (mysqli_num_rows($sresults) == 0){
 									echo '<div class=\"list-group\">
@@ -822,7 +829,14 @@ if(settings($conn, 'language') == "sk" || settings($conn, 'language') == "de") {
 									</li>
 								</ul>';
 							}else{
-								$squery = "SELECT * FROM schedule_daily_time_zone_view where zone_id ='{$zone_id}' AND tz_status = 1 AND time_status = '1' AND (WeekDays & (1 << {$dow})) > 0 ORDER BY start asc";
+//								$squery = "SELECT * FROM schedule_daily_time_zone_view where zone_id ='{$zone_id}' AND tz_status = 1 AND time_status = '1' AND (WeekDays & (1 << {$dow})) > 0 ORDER BY start asc";
+                                                                $squery = "SELECT schedule_daily_time.sch_name, schedule_daily_time.start, schedule_daily_time.end,
+                                                                schedule_daily_time_zone.temperature, schedule_daily_time_zone.id AS tz_id
+                                                                FROM `schedule_daily_time`, `schedule_daily_time_zone`
+                                                                WHERE (schedule_daily_time.id = schedule_daily_time_zone.schedule_daily_time_id) AND schedule_daily_time.status = 1
+                                                                AND schedule_daily_time_zone.status = 1 AND schedule_daily_time.type = 0 AND schedule_daily_time_zone.zone_id ='{$zone_id}'
+                                                                AND (schedule_daily_time.WeekDays & (1 << {$dow})) > 0
+                                                                ORDER BY schedule_daily_time.start asc;";
 								$sresults = $conn->query($squery);
 								if (mysqli_num_rows($sresults) == 0){
 									echo '<div class=\"list-group\">
