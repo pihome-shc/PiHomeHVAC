ee9e14bbb6da2830e4d9c52dff7b7bc944c03301
twa127 <46624596+twa127@users.noreply.github.com>
2022-02-24 07:34:37 +0000

    Update controller.php

--- cron/controller.php
+++ cron/controller.php
@@ -492,7 +492,8 @@ while ($row = mysqli_fetch_assoc($results)) {
 
                                 // if add-on controller then process state change from GUI or api call
                                 if ($zone_category == 2) {
-                                        $current_state = $zone_controllers[$crow]["zone_controller_current_state"];
+//                                        $current_state = $zone_controllers[$crow]["zone_controller_current_state"];
+                                        $current_state = $zone_status_prev;
                                         $add_on_state = $zone_controllers[$crow]["zone_controller_state"];
                                         $zone_controler_child_id = $zone_controllers[$crow]["controler_child_id"];
                                         if ($zone_current_mode == "74" || $zone_current_mode == "75") {
