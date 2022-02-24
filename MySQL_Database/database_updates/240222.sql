e4112320857181f46c95e21eda45e5e77153f636
twa127 <46624596+twa127@users.noreply.github.com>
2022-02-24 07:50:16 +0000

    Update 240222.sql

--- MySQL_Database/database_updates/240222.sql
+++ MySQL_Database/database_updates/240222.sql
@@ -2,4 +2,5 @@ Drop View if exists messages_in_view_24h;
 CREATE VIEW messages_in_view_24h AS
 select id, node_id, child_id, datetime, payload
 from messages_in
-where datetime > DATE_SUB( NOW(), INTERVAL 24 HOUR);
+where datetime > DATE_SUB( NOW(), INTERVAL 24 HOUR)
+order by id desc;
