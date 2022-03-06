31a83b260db5a85a25fa296b594672a60081b778
twa127 <46624596+twa127@users.noreply.github.com>
2022-03-06 15:30:26 +0000

    Update lv.php

--- languages/lv.php
+++ languages/lv.php
@@ -830,4 +830,5 @@ $lang['zone_boost_update_fail'] = "Zone Boost Record Update Failed.";
 $lang['away_enable'] = "Enable Away Schedule";
 $lang['away_setup'] = "Setup Away Function";
 $lang['zone_operating_mode_numbering'] = "Zone Operating Mode Numbering";
+$lang['sensor_last24h'] = "Senors Readings Last 24 Hours for Sensor ID - ";
 ?>
