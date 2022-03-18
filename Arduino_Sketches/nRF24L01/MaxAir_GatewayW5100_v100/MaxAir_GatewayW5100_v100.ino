9fa486806585f8224240f04fec6bc664b40b89f7
twa127 <46624596+twa127@users.noreply.github.com>
2022-03-18 09:50:26 +0000

    Update MaxAir_GatewayW5100_v100.ino

--- Arduino_Sketches/nRF24L01/MaxAir_GatewayW5100_v100/MaxAir_GatewayW5100_v100.ino
+++ Arduino_Sketches/nRF24L01/MaxAir_GatewayW5100_v100/MaxAir_GatewayW5100_v100.ino
@@ -28,7 +28,7 @@
 #define SKETCH_VERSION "1.0"
 
 // Enable and select radio type attached
-#define MY_RADIO_NRF24
+#define MY_RADIO_RF24
 //#define MY_RADIO_NRF5_ESB
 //#define MY_RADIO_RFM69
 //#define MY_RADIO_RFM95
