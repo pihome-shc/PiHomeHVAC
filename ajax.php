afe859c2332f013bc8a79c07deb242263f0e37f2
twa127 <46624596+twa127@users.noreply.github.com>
2022-04-08 21:32:21 +0100

    Add files via upload

--- ajax.php
+++ ajax.php
@@ -1021,15 +1021,17 @@ function GetModal_Sensors($conn)
                                 $query = "SELECT payload FROM messages_in where node_id = '{$nrow['node_id']}' AND child_id = {$row['sensor_child_id']} ORDER BY datetime DESC LIMIT 1;";
                                 $mresult = $conn->query($query);
 				$mcount = mysqli_num_rows($mresult);
+				$unit = SensorUnits($conn,$row['sensor_type_id']);
 				if ($mcount > 0) { $mrow = mysqli_fetch_array($mresult); }
 				echo '<div class="list-group-item">
-					<div class="form-group row">
-  						<div class="col-xs-4">&nbsp&nbsp'.$nrow['node_id'].'_'.$row['sensor_child_id'].'</div>
-						<div class="col-xs-2">'.$row['name'].'</div>';
-						echo '<div class="col-xs-2"><i class="fa fa-battery-full"></i> '.round($brow ['bat_level'],0).'% - '.$brow ['bat_voltage'].'</div>';
-						if ($mcount > 0) { echo '<div class="col-xs-1"><i class="ionicons ion-thermometer red"></i> - '.$mrow['payload'].'&deg</div>'; } else { echo '<div class="col-xs-2"></div>'; }
-        					echo '<div class="col-xs-2"><span class="pull-right text-muted small"><button type="button"  data-remote="false" data-target="#ajaxModal" data-ajax="ajax.php?Ajax=GetModal_SensorsInfo&id=' . $nrow['node_id'] . '" onclick="sensors_Info(this);"><em>'.$nrow['last_seen'].'&nbsp</em></span></button></div>
-					</div>
+                                        <div class="form-group row" style="height: 10px;">
+                                                <div class="col-xs-6">&nbsp&nbsp'.$nrow['node_id'].'_'.$row['sensor_child_id'].' - '.$row['name'].'</div>
+                                                <div class="col-xs-6"><span class="pull-right text-muted small"><button type="button"  data-remote="false" data-target="#ajaxModal" data-ajax="ajax.php?Ajax=GetModal_SensorsInfo&id=' . $nrow['node_id'] . '" onclick="sensors_Info(this);"><em>'.$nrow['last_seen'].'&nbsp</em></span></button>&nbsp&nbsp</div>
+                                        </div>
+					<div class="form-group row" style="height: 10px;">
+						<div class="col-xs-3">&nbsp&nbsp<i class="fa fa-battery-full"></i> '.round($brow ['bat_level'],0).'% - '.$brow ['bat_voltage'].'</div>';
+						if ($mcount > 0) { echo '<div class="col-xs-3"><i class="ionicons ion-thermometer red"></i> - '.$mrow['payload'].$unit.'</div>'; } else { echo '<div class="col-xs-2"></div>'; }
+					echo '</div>
 				</div> ';
 			}
 		echo '</div>'; //close class=\"list-group\">
