d3e38d33b8642ee3ac18f85a37abe0c84d4825dd
twa127 <46624596+twa127@users.noreply.github.com>
2022-04-17 08:40:22 +0100

    Update ajax_fetch_data.php

--- ajax_fetch_data.php
+++ ajax_fetch_data.php
@@ -632,15 +632,27 @@ if ($type <= 5 || $type == 8) {
                         }
                 }
         }
-	if($row["time_status"]=="0"){ $shactive="bluesch"; }else{ $shactive="orangesch"; }
-        if ((($end_time > $start_time && $time > $start_time && $time < $end_time && ($row["WeekDays"]  & (1 << $dow)) > 0) || ($end_time < $start_time && $time < $end_time && ($row["WeekDays"]  & (1 << $prev_dow)) > 0) || ($end_time < $start_time && $time > $start_time && ($row["WeekDays"]  & (1 << $dow)) > 0)) && $row["time_status"]=="1") {
-        	if (($sch_type == 1 && $away_status == 1) || ($sch_type == 0 && $away_status == 0)) { $shactive="redsch"; }
+        $query = "SELECT MIN(`disabled`) AS all_disabled FROM `schedule_daily_time_zone` WHERE `schedule_daily_time_id` = {$id};";
+        $result = $conn->query($query);
+        $sdrow = mysqli_fetch_array($result);
+        if($row["time_status"]=="0"){ $shactive="bluesch"; }else{ $shactive="orangesch"; }
+        if ((($end_time > $start_time && $time > $start_time && $time < $end_time && ($row["WeekDays"]  & (1 << $dow)) > 0) || ($end_time < $start_time && $time < $end_time && ($row["WeekD>
+                if (($sch_type == 1 && $away_status == 1) || ($sch_type == 0 && $away_status == 0)) { $shactive="redsch"; }
         }
-        echo '<div class="circle ' . $shactive . '">';
-        	if($row["category"] <> 2 && $row["sensor_type_id"] <> 3) {
-                        $unit = SensorUnits($conn,$row['sensor_type_id']);
-                        echo '<p class="schdegree">' . DispSensor($conn, number_format($row["max_c"], 1), $row["sensor_type_id"]) . $unit . '</p>';
-                }
-      	echo ' </div>';
+        if ($row["time_status"] == 0 || $sdrow["all_disabled"] == 1) {
+                echo '<div class="circle bluesch_disable">';
+                        if($row["category"] <> 2 && $row["sensor_type_id"] <> 3) {
+                                $unit = SensorUnits($conn,$row['sensor_type_id']);
+                                echo '<p class="schdegree">D</p>';
+                        }
+                echo ' </div>';
+        } else {
+                echo '<div class="circle ' . $shactive . '">';
+                        if($row["category"] <> 2 && $row["sensor_type_id"] <> 3) {
+                                $unit = SensorUnits($conn,$row['sensor_type_id']);
+                                echo '<p class="schdegree">' . DispSensor($conn, number_format($row["max_c"], 1), $row["sensor_type_id"]) . $unit . '</p>';
+                        }
+                echo ' </div>';
+        }
 }
 ?>
