e97b985165c60dde951a8e916f74589306a51637
twa127 <46624596+twa127@users.noreply.github.com>
2022-04-19 09:26:09 +0100

    Add files via upload

--- cron/login.py
+++ cron/login.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
-# add following line to show up when some one ssh to pi /etc/profile 
+# add following line to show up when some one ssh to pi /etc/profile
 # sudo python /var/www/cron/login.py
-# clear everything from /etc/motd to remove generic message. 
+# clear everything from /etc/motd to remove generic message.
 import socket, os, re, time, sys, subprocess, fcntl, struct
 from threading import Thread
 class bc:
@@ -11,7 +11,8 @@ class bc:
 	WARN = '\033[0;31;40m'
 	GREEN = '\033[0;32;40m'
 	org = '\033[91m'
-print bc.HEADER + " "	
+	hed = "\033[95m"
+print(bc.hed + " ")
 print("    __  __                             _         ")
 print("   |  \/  |                    /\     (_)        ")
 print("   | \  / |   __ _  __  __    /  \     _   _ __  ")
@@ -19,30 +20,30 @@ print("   | |\/| |  / _` | \ \/ /   / /\ \   | | | '__| ")
 print("   | |  | | | (_| |  >  <   / ____ \  | | | |    ")
 print("   |_|  |_|  \__,_| /_/\_\ /_/    \_\ |_| |_|    ")
 print(" ")
-print("             " +bc.SUB + "S M A R T   THERMOSTAT " + bc.ENDC)
-print bc.WARN +" "
-print "*************************************************************************"
-print "* MaxAir is a Linux based Central Heating Control systems. It runs from *"
-print "* a web interface and it comes with ABSOLUTELY NO WARRANTY, to the      *"
-print "* extent permitted by applicable law. I take no responsibility for any  *"
-print "* loss or damage to you or your property.                               *"
-print "* DO NOT MAKE ANY CHANGES TO YOUR HEATING SYSTEM UNTILL UNLESS YOU KNOW *"
-print "* WHAT YOU ARE DOING                                                    *"
-print "*************************************************************************"
-print bc.GREEN +"                                                      Have Fun - PiHome"  + bc.ENDC
+print("        " + bc.SUB + "S M A R T   T H E R M O S T A T " + bc.ENDC)
+print(bc.WARN + " ")
+print("********************************************************")
+print("* MySensors Wifi/Ethernet/Serial Gateway Communication *")
+print("* Script to communicate with MySensors Nodes, for more *")
+print("* info please check MySensors API.                     *")
+print("*      Build Date: 18/09/2017                          *")
+print("*      Version 0.12 - Last Modified 31/01/2022         *")
+print("*                                 Have Fun - PiHome.eu *")
+print("********************************************************")
+print(" " + bc.ENDC)
 
-df = subprocess.Popen(["df", "-h"], stdout=subprocess.PIPE)
+df = subprocess.Popen(["df","-h"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 output = df.communicate()[0]
 device, size, used, available, percent, mountpoint = \
 	output.split("\n")[1].split()
-print bc.org +"Disk/SD Card Usage" + bc.ENDC
-print "Filesystem  Size  Used   Avail  Used%"
-print device+"   "+size+"   "+used+"   "+available+"   "+percent
+print(bc.org +"Disk/SD Card Usage" + bc.ENDC)
+print("Filesystem  Size  Used   Avail  Used%")
+print(device+"   "+size+"   "+used+"   "+available+"   "+percent)
 
 def get_interface_ip(ifname):
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s', ifname[:15]))[20:24])
-	
+	return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s', bytes(ifname[:15], 'utf-8')))[20:24])
+
 def get_ip():
 	ip = socket.gethostbyname(socket.gethostname())
 	if ip.startswith("127."):
@@ -54,5 +55,5 @@ def get_ip():
 			except IOError:
 				pass
 	return ip
-print "WebServer:  "+bc.GREEN +"http://"+str(get_ip())+"/"+ bc.ENDC
-print "PhpMyAdmin: "+bc.GREEN +"http://"+str(get_ip())+"/phpmyadmin"+ bc.ENDC
+print ("WebServer:  "+bc.GREEN +"http://"+str(get_ip())+"/"+ bc.ENDC)
+print ("PhpMyAdmin: "+bc.GREEN +"http://"+str(get_ip())+"/phpmyadmin"+ bc.ENDC)
