f58f873ad318185a33ceea0a4f19a47764b5d966
twa127 <46624596+twa127@users.noreply.github.com>
2022-04-23 11:22:42 +0100

    Update en.php

--- languages/en.php
+++ languages/en.php
@@ -877,6 +877,6 @@ $lang['continous'] = "Continous";
 $lang['onchange'] = "On-Change";
 $lang['correct_factor'] = "Correct Factor";
 $lang['res'] = "Res +/-";
-$lang['sensor_resolution'] = "Sensor Resolution";
-$lang['sensor_resolution_info'] = "Resolution between +/- 0.0 to 1.0"; 
+$lang['sensor_resolution'] = "On-Change Sensor Resolution";
+$lang['sensor_resolution_info'] = "Set On-Change Resolution between +/- 0.0 to 1.0"; 
 ?>
