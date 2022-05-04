69e084758d2ce1b103bda7786036c5dc674d5229
twa127 <46624596+twa127@users.noreply.github.com>
2022-05-04 11:56:34 +0100

    Add files via upload

--- logout.php
+++ logout.php
@@ -21,6 +21,8 @@
 // Five steps to closing a session
 // (i.e. logging out)
 
+require_once(__DIR__.'/st_inc/connection.php');
+
 // 1. Find the session
 session_start();
 
@@ -28,9 +30,9 @@ session_start();
 $query = "SELECT id, s_id FROM userhistory ORDER BY id DESC;";
 $results = $conn->query($query);
 if (mysqli_num_rows($results) > 0) {
-        while ($row = mysqli_fetch_assoc($results)) {
-                if (password_verify(session_id(), $row['s_id'])) { // check if this session exists in the userhistory table
-                        $query = "UPDATE userhistory SET logged_out = NOW() WHERE id = {$row['id']};";
+	while ($row = mysqli_fetch_assoc($results)) {
+        	if (password_verify(session_id(), $row['s_id'])) { // check if this session exists in the userhistory table
+                	$query = "UPDATE userhistory SET logged_out = NOW() WHERE id = {$row['id']};";
                         $conn->query($query);
                         break;
                 }
@@ -42,7 +44,7 @@ $_SESSION = array();
 
 // 4. Destroy the session cookies
 if(isset($_COOKIE[session_name()])) {
-        setcookie(session_name(), '', time()-42000, '/');
+	setcookie(session_name(), '', time()-42000, '/');
 }
 if(isset($_COOKIE['maxair_login'])) {
         setcookie('maxair_login', '', time()-42000, '/');
