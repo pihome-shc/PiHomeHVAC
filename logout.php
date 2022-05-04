c50b50692cef3781a964b32ef7dbbb3e19725a9b
twa127 <46624596+twa127@users.noreply.github.com>
2022-05-04 11:22:34 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -18,24 +18,37 @@
 * WHAT YOU ARE DOING                                                    *"
 *************************************************************************"
 */
-// Four steps to closing a session
+// Five steps to closing a session
 // (i.e. logging out)
 
 // 1. Find the session
 session_start();
 
-// 2. Unset all the session variables
+// 2. Mark the userhistory with logged out time
+$query = "SELECT id, s_id FROM userhistory ORDER BY id DESC;";
+$results = $conn->query($query);
+if (mysqli_num_rows($results) > 0) {
+        while ($row = mysqli_fetch_assoc($results)) {
+                if (password_verify(session_id(), $row['s_id'])) { // check if this session exists in the userhistory table
+                        $query = "UPDATE userhistory SET logged_out = NOW() WHERE id = {$row['id']};";
+                        $conn->query($query);
+                        break;
+                }
+        }
+}
+
+// 3. Unset all the session variables
 $_SESSION = array();
 
-// 3. Destroy the session cookie
+// 4. Destroy the session cookies
 if(isset($_COOKIE[session_name()])) {
-	setcookie(session_name(), '', time()-42000, '/');
+        setcookie(session_name(), '', time()-42000, '/');
 }
 if(isset($_COOKIE['maxair_login'])) {
         setcookie('maxair_login', '', time()-42000, '/');
 }
 
-// 4. Destroy the session
+// 5. Destroy the session
 session_destroy();
 
 header("Location: index.php?logout=1");
