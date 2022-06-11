c82e91b6047465e605c662a3f2726adfdef6eddd
twa127 <46624596+twa127@users.noreply.github.com>
2022-06-11 09:11:14 +0100

    Delete MySQL_Database/database_update directory

--- MySQL_Database/database_update/110622.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `theme`(`id`, `sync`, `purge`, `name`, `row_justification`, `color`, `text_color`, `tile_size`)
-VALUES (15,0,0,'Burnt Orange Left','left','orange-red','text-white','0');
-INSERT INTO `theme`(`id`, `sync`, `purge`, `name`, `row_justification`, `color`, `text_color`, `tile_size`)
-VALUES (16,0,0,'Burnt Orange Center','center','orange-red','text-white','0');
