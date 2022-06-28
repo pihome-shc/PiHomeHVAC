f7cd50ca9460b5f655920fe8d480a586669c217c
twa127 <46624596+twa127@users.noreply.github.com>
2022-06-28 09:44:50 +0100

    Create 280622.sql

---
+++ MySQL_Database/database_updates/280622.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `auto_backup` ADD COLUMN IF NOT EXISTS `last_backup` DATETIME NOT NULL AFTER `email_confirmation`;
+UPDATE `auto_backup` SET `last_backup` = DATE_SUB(NOW(), INTERVAL 2 DAY);
