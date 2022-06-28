628f8b976371e0bf259be4aac09594a664d22583
twa127 <46624596+twa127@users.noreply.github.com>
2022-06-28 09:49:04 +0100

    Update 270622.sql

--- MySQL_Database/database_updates/270622.sql
+++ MySQL_Database/database_updates/270622.sql
@@ -1,2 +1,11 @@
-ALTER TABLE `auto_backup` ADD COLUMN IF NOT EXISTS `last_backup` DATETIME NOT NULL AFTER `email_confirmation`;
-UPDATE `auto_backup` SET `last_backup` = DATE_SUB(NOW(), INTERVAL 1 DAY);
+DROP TABLE IF EXISTS `repository`;
+CREATE TABLE IF NOT EXISTS `repository` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sync` tinyint(4) NOT NULL,
+  `purge` tinyint(4) NOT NULL COMMENT 'Mark For Deletion',
+  `status` tinyint(4) NOT NULL,
+  `name` char(50) COLLATE utf16_bin,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+INSERT INTO `repository` (`sync`, `purge`, `status`, `name`)
+VALUES (0, 0, 1, 'pihome-shc/PiHomeHVAC'), (0, 0, 0, 'twa127/PiHomeHVAC'), (0, 0, 0, 'dvdcut/PiHomeHVAC'), (0, 0, 0, 'JSa1987/PiHomeHVAC'), (0, 0, 0, 'mjhumphrey/PiHomeHVAC'), (0, 0, 0, 'sandreialexandru/PiHomeHVAC');
