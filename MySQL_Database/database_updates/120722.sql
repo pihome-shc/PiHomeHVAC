35aa7ff715b563da31268afafa0020dbe01118a2
twa127 <46624596+twa127@users.noreply.github.com>
2022-07-13 07:51:44 +0100

    Update 120722.sql

--- MySQL_Database/database_updates/120722.sql
+++ MySQL_Database/database_updates/120722.sql
@@ -1,1 +1,5 @@
-INSERT INTO `zone_type`(`sync`, `purge`, `type`, `category`) VALUES (0, 0, 'Cooling', 5);
+INSERT INTO `zone_type`(`sync`, `purge`, `type`, `category`)
+SELECT * FROM (SELECT '0' AS `sync`, '0' AS `purge`, 'Cooling' AS `type`, '5' AS `category`) AS tmp
+WHERE NOT EXISTS (
+    SELECT `type` FROM `zone_type` WHERE `type` = 'Cooling'
+) LIMIT 1;
