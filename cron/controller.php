fd5b7f91179d9bb011ba1c0368eb303aab5f69bb
twa127 <46624596+twa127@users.noreply.github.com>
2022-07-15 08:26:32 +0100

    Update controller.php

--- cron/controller.php
+++ cron/controller.php
@@ -1571,7 +1571,7 @@ while ($row = mysqli_fetch_assoc($results)) {
 					$add_on_stop_cause="Away Active";
 					$zone_state = 0;
 				}
-			} else { // process Zone Category 2 or 4 zone
+			} else { // process Zone Category 2 Switch type zone
 				if ($away_status=='1' && $away_sch == 0){
 					$zone_status="0";
 					$zone_mode = 90;
