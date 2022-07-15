48e578264394a05381c0deaf438c835ac1858f52
twa127 <46624596+twa127@users.noreply.github.com>
2022-07-15 18:04:36 +0100

    Update sk.php

--- languages/sk.php
+++ languages/sk.php
@@ -950,5 +950,5 @@ $lang['restore_db_warning'] = "This Action will Overwrite Your Current DB. DO YO
 $lang['setup_database_restore'] = "Setup Database Restore";
 $lang['zone_category5'] = "Controller, Sensor and NO System Control (Negative Sensor Gradient)";
 $lang['zone_category6'] = "Controller, Binary Sensor and NO System Control";
-$lang['cooling_zone'] = "Cooling Type Zone";
+$lang['negative_gradient_zone'] = "Zones With a Negative Control Gradient";
 ?>
