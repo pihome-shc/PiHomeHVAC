5c23eb4336f1f77b4c1c4c209ccaf227cae0a87c
twa127 <46624596+twa127@users.noreply.github.com>
2022-08-07 14:34:10 +0100

    Create 070822.sql

---
+++ MySQL_Database/database_updates/070822.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `ebus_messages`;
+CREATE TABLE IF NOT EXISTS `ebus_messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sync` tinyint(4) NOT NULL,
+  `purge` tinyint(4) NOT NULL COMMENT 'Mark For Deletion',
+  `message` char(50) COLLATE utf8_bin,
+  `sensor_id` int(11) NOT NULL,
+  `position` int(11) NOT NULL,
+  `offset` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+INSERT INTO `jobs`(`job_name`, `script`, `enabled`, `log_it`, `time`, `output`, `datetime`) VALUES ('ebus','/var/www/cron/check_ebus.php',0,0,60,'',now());
