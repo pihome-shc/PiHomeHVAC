938a07fc878c0f47b7c3e4dafefc9071c0ecc0b1
twa127 <46624596+twa127@users.noreply.github.com>
2022-08-08 21:28:35 +0100

    Update check_ebus.php

--- cron/check_ebus.php
+++ cron/check_ebus.php
@@ -12,7 +12,7 @@ echo " \033[0m \n";
 echo "                \033[45m S M A R T   T H E R M O S T A T \033[0m \n";
 echo "\033[31m";
 echo "**********************************************************\n";
-echo "*      EBus Script Version 0.1 Build Date 05/09/2021     *\n";
+echo "*      eBUS Script Version 0.1 Build Date 05/09/2021     *\n";
 echo "*          Last Modification Date 06/08/2022             *\n";
 echo "*                                Have Fun - PiHome.eu    *\n";
 echo "**********************************************************\n";
@@ -27,34 +27,34 @@ $date_time = date('Y-m-d H:i:s');
 $ebus_script_txt = 'python3 /var/www/cron/ebus/ebus.py';
 $line = "--------------------------------------------------------------------------\n";
 
-echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Python EBus Script Status Check Script Started \n"; 
+echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Python eBUS Script Status Check Script Started \n"; 
 
 // Checking if GPIO Switch script is running
 exec("ps ax | grep '$ebus_script_txt' | grep -v grep", $pids);
 $nopids = count($pids);
 if($nopids==0) { // Script not running
-	echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Python  EBus Script \033[41mNot Running\033[0m \n";
-	echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Starting Python Script for Ebus \n";
+	echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Python  eBUS Script \033[41mNot Running\033[0m \n";
+	echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Starting Python Script for eBUS \n";
 	exec("$ebus_script_txt </dev/null >/dev/null 2>&1 & ");
 	exec("ps aux | grep '$ebus_script_txt' | grep -v grep | awk '{ print $2 }' | head -1", $out);
 	echo "\033[36m".date('Y-m-d H:i:s')."\033[0m - The PID is: \033[41m".$out[0]."\033[0m \n";
 } else {
-	if($nopids>1) { // Proceed if more than one GPIO Switch  script running
-		echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Multiple EBus Scripts are Detected \033[41m$nopids\033[0m \n";
+	if($nopids>1) { // Proceed if more than one eBUS script running
+		echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Multiple eBUS Scripts are Detected \033[41m$nopids\033[0m \n";
 		$regex = preg_quote($ebus_script_txt, '/');
 		exec("ps -eo s,pid,cmd | grep 'T.*$regex' | grep -v grep | awk '{ print $2 }'", $tpids);
 		$notpids=count($tpids);
 		echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Number of Terminated Script Killed \033[41m$notpids\033[0m \n";
 		foreach($tpids as $tpid){
-			exec("kill -9 $tpid 2> /dev/null"); // Kill all GPIO Switch script ghost processes (in stat "T"(Terminated)). Common occurrence after running script in terminal and terminating by Ctrl+z
+			exec("kill -9 $tpid 2> /dev/null"); // Kill all eBUS script ghost processes (in stat "T"(Terminated)). Common occurrence after running script in terminal and terminating by Ctrl+z
 		}
 		if($nopids-$notpids>1 || $nopids-$notpids==0) { // Proceed if none or more than one script runs
-			if($nopids-$notpids>1) { // Proceed if more than one active GPIO Switch  script 
+			if($nopids-$notpids>1) { // Proceed if more than one active eBUS script 
 				exec("ps -eo s,pid,cmd | grep '$ebus_script_txt' | grep -v grep | awk '{ print $2 }'", $tpids);
 				$notpids=$nopids-$notpids;
-				echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Multiple Active EBus Script are Running \033[41m$notpids\033[0m \n";
+				echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Multiple Active eBUS Script are Running \033[41m$notpids\033[0m \n";
 				foreach($tpids as $tpid){
-					exec("kill -9 $tpid 2> /dev/null"); // Kill all GPIO Switch scripts
+					exec("kill -9 $tpid 2> /dev/null"); // Kill all eBUS scripts
 				}
 			}
 			echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - All Script Killed. Started New \n";
@@ -62,11 +62,11 @@ if($nopids==0) { // Script not running
 			exec("ps aux | grep '$ebus_script_txt' | grep -v grep | awk '{ print $2 }' | head -1", $out);
 		}
 	}
-	echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Python EBus Script is \033[42mRunning\033[0m \n";
+	echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Python eBUS Script is \033[42mRunning\033[0m \n";
 	exec("ps -eo s,pid,cmd | grep '$ebus_script_txt' | grep -v grep | awk '{ print $2 }' | head -1", $out);
 	echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - The PID is: \033[42m" . $out[0]."\033[0m \n";
 }
-echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Python EBus Script Status Check Script Ended \n"; 
+echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Python eBUS Script Status Check Script Ended \n"; 
 echo "\033[32m***************************************************************************\033[0m";
 echo "\n";
 ?>
