a7598fd894407e0a12dd4ebf94945e0deed97d52
twa127 <46624596+twa127@users.noreply.github.com>
2022-10-21 22:08:25 +0100

    Update 221022.sql

--- MySQL_Database/database_updates/221022.sql
+++ MySQL_Database/database_updates/221022.sql
@@ -9,4 +9,3 @@ CREATE TABLE IF NOT EXISTS `ebus_messages` (
   `offset` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-INSERT INTO `ebus_messages` VALUES (1,0,0,'FlowTemp',44,0,0),(2,0,0,'ReturnTemp',45,0,0),(3,0,0,'Flame',70,0,90),(4,0,0,'StateNumber',70,1,0);
