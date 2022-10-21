acc3c495baf73fcd8eee4f84ba1d686ec0190b98
twa127 <46624596+twa127@users.noreply.github.com>
2022-10-21 11:39:47 +0100

    Update install.sh

--- add_on/Image_Tools/install.sh
+++ add_on/Image_Tools/install.sh
@@ -8,13 +8,10 @@ echo "Create 'auto_image' table and default entry. Updating the Job in the datab
 cd /var/www/add_on/Image_Tools
 python3 db_config.py
 
-echo "Download tools from GitHub to /usr/local/bin and set permisions"
-mkdir ./tools
-git clone https://github.com/scruss/RonR-RaspberryPi-image-utils.git "./tools"
+echo "Copy scripts to /usr/local/bin and set permisions"
 
-chown -R www-data:www-data ./tools
-chmod +x ./tools/image-*
-sudo cp -p tools/image-* /usr/local/bin
-sudo rm -R ./tools
+chown -R www-data:www-data ./image_utils
+chmod +x ./image_utils/image-*
+sudo cp -p ./image_utils/image-* /usr/local/bin
 
 echo "Done"
