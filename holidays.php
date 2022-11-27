f2057f558843fba928cae4a1556a35433438cd5b
twa127 <46624596+twa127@users.noreply.github.com>
2022-11-27 18:25:21 +0000

    Update holidays.php

--- holidays.php
+++ holidays.php
@@ -33,7 +33,7 @@ require_once(__DIR__.'/st_inc/functions.php');
                 	<div id="holidayslist" >
                                 <div class="d-flex justify-content-center" style="margin-top:10px"><?php echo $lang['please_wait_text']; ?></div>
                                 <div class="d-flex justify-content-center" style="margin-top:10px">
-                                        <div class="spinner-border text-<?php echo explode('-', theme($conn, settings($conn, 'theme'), 'background_color'))[1]; ?>"
+                                        <div class="spinner-border text-<?php echo theme($conn, settings($conn, 'theme'), 'color'); ?>"
                                                 role="status">
                                         </div>
                                 </div>
