bb9002f2bc39c3a192f36a21824151203b1d5351
twa127 <46624596+twa127@users.noreply.github.com>
2022-12-08 20:53:44 +0000

    Update 081222.sql

--- MySQL_Database/database_updates/081222.sql
+++ MySQL_Database/database_updates/081222.sql
@@ -3,14 +3,14 @@ CREATE TABLE IF NOT EXISTS `relay_logs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `sync` tinyint(4) NOT NULL,
   `purge` tinyint(4) NOT NULL COMMENT 'Mark For Deletion',
-  `relay_id' int(11) NOT NULL,
+  `relay_id` int(11) NOT NULL,
   `relay_name` char(50) COLLATE utf8_bin,
   `message` varchar(100) CHARACTER SET utf8 NOT NULL COMMENT 'Sent To Relay',
   `zone_name` char(50) COLLATE utf8_bin,
-  `zone_mode' int(11) NOT NULL,
+  `zone_mode` int(11) NOT NULL,
   `datetime` timestamp NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 ALTER TABLE `db_cleanup` ADD `relay_logs` CHAR(50) CHARACTER SET utf16 COLLATE utf16_bin NULL DEFAULT NULL AFTER `gateway_logs`;
 
