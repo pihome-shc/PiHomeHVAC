c1776a458339a66046c42f59d2dcfb09551cf860
twa127 <46624596+twa127@users.noreply.github.com>
2023-03-24 18:24:41 +0000

    Delete shutdown.py

--- shutdown.py
+++
@@ -1,5 +0,0 @@
-command = "/usr/bin/sudo /sbin/shutdown -h now"
-import subprocess
-process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-output = process.communicate()[0]
-print(output)
