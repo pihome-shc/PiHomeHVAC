209674cfb6feabd5cbea2e26c4f68dc113b93a70
twa127 <46624596+twa127@users.noreply.github.com>
2023-03-25 07:47:46 +0000

    Update settingslist.php

--- settingslist.php
+++ settingslist.php
@@ -130,7 +130,7 @@ if ($settings_id <= 3) {
 
                                                 <button type="button" class="btn btn-bm-<?php echo theme($conn, $theme, 'color'); ?> btn-circle <?php echo $button_style;?> mainbtn animated fadeIn" data-bs-toggle="modal" data-bs-target="#status_scripts">
                                                 <h3 class="buttontop"><small><?php echo $lang['scripts_status']; ?></small></h3>
-                                                <h3 class="degre" ><i class="bi bi-heart-pulse-fill red" style="font-size: 1.5rem;"></i></h3>
+                                                <h3 class="degre" ><i class="bi bi-activity red" style="font-size: 1.5rem;"></i></h3>
                                                 <h3 class="status"></small></h3>
                                                 </button>
 
