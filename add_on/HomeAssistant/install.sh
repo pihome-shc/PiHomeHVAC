f0a928870683cc3c674b83b4fd2edaf66390b1fe
twa127 <46624596+twa127@users.noreply.github.com>
2024-05-27 09:54:42 +0100

    Update install.sh

--- add_on/HomeAssistant/install.sh
+++ add_on/HomeAssistant/install.sh
@@ -3,15 +3,6 @@
 #app_name:Install Home Assistant Integration
 #app_description:This add-on requires Home Assistant with Mosquito Broker running on a separate device.
 
-MODEL=$(tr -d '\0' </proc/device-tree/model)
-if [[ $MODEL == *"Raspberry"* ]]; then
-  echo "Model: Raspberry"
-  REQUIREMENTS=/var/www/add_on/HomeAssistant/requirements_RPi.txt
-else
-  echo "Model: generic SBC"
-  REQUIREMENTS=/var/www/add_on/HomeAssistant/requirements.txt
-fi
-
 PYTHON3DEV=$( apt-cache pkgnames python3-dev )
 if [[ $PYTHON3DEV == *"python3-dev"* ]]; then
   echo "Python3-dev is already installed"
@@ -21,7 +12,20 @@ else
 fi
 
 echo "Installing Phyton modules"
-sudo pip3 install -r $REQUIREMENTS --break-system-packages
+MODEL=$(tr -d '\0' </proc/device-tree/model)
+if [[ $MODEL == *"Raspberry"* ]]; then
+  echo "Model: Raspberry"
+  sudo pip3 install paho-mqtt==1.5.0 --break-system-packages
+  sudo pip3 install psutil==5.6.6 --break-system-packages
+  sudo pip3 install pytz==2019.2 --break-system-packages
+  sudo pip3 install PyYAML==5.4 --break-system-packages
+  sudo pip3 install rpi_bad_power==0.1.0 --break-system-packages
+else
+  sudo pip3 install paho-mqtt==1.5.0
+  sudo pip3 install psutil
+  sudo pip3 install pytz
+  sudo pip3 install PyYAML
+fi
 
 echo "Creating service for auto start"
 sudo cp /var/www/add_on/HomeAssistant/HA_integration.service /etc/systemd/system/HA_integration.service
