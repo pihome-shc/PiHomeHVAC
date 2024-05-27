7dca5bc397036f3539a0ecefbdc01bbc29e88267
twa127 <46624596+twa127@users.noreply.github.com>
2024-05-27 19:17:25 +0100

    Update Maxair_Multi_Controller_v034.ino

--- Arduino_Sketches/nRF24L01/Maxair_Multi_Controller_v034/Maxair_Multi_Controller_v034.ino
+++ Arduino_Sketches/nRF24L01/Maxair_Multi_Controller_v034/Maxair_Multi_Controller_v034.ino
@@ -9,7 +9,7 @@
 // *****************************************************************
 // *          8 Channel Multi Controller Relay Sketch              *
 // *            Version 0.34 Build Date 06/11/2017                 *
-// *            Last Modification Date 23/05/2024                  *
+// *            Last Modification Date 27/05/2024                  *
 // *                                          Have Fun - PiHome.eu *
 // *****************************************************************
 
@@ -202,6 +202,10 @@ void loop(){
     // If 8 Releay controller, the add A0 and A1 pin states
     if (NUMBER_OF_RELAYS == 8) {
       port_mask = port_mask + ((PORTC & 0b00000011) << 6);
+    }
+    // Invert bits depending on value of 'trigger'
+    if (trigger == 0) {
+      port_mask = ~port_mask;
     }
     #ifdef MY_DEBUG
       Serial.print("Current Relay States: ");
