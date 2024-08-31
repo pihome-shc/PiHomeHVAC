8dffeee3d53a93b4915501610ef7d5a4f7360766
twa127 <46624596+twa127@users.noreply.github.com>
2024-08-31 18:01:18 +0100

    Update check_gw.php

--- cron/check_gw.php
+++ cron/check_gw.php
@@ -130,8 +130,10 @@ if ($gw_status == '0') {
 		echo "\033[36m".date('Y-m-d H:i:s'). "\033[0m - Starting Python Script for Gateway \n";
 		exec("$gw_script_txt </dev/null >/dev/null 2>&1 & ");
                 $out = [];
-                while (count($out) == 0) {;
+		$cnt = 0;
+                while (count($out) == 0 && $cnt < 10) {;
                         exec("ps aux | grep '$gw_script_txt' | grep -v grep | awk '{ print $2 }' | head -1", $out);
+			$cnt = $cnt + 1;
                 }
 		echo "\033[36m".date('Y-m-d H:i:s')."\033[0m - The PID is: \033[41m".$out[0]."\033[0m \n";
 		$pid_details = exec("ps -p '$out[0]' -o lstart=");
