51013a7bb627b940d79c8c3bffe758e9091d19e7
twa127 <46624596+twa127@users.noreply.github.com>
2024-12-23 12:27:27 +0000

    Update db.php

--- db.php
+++ db.php
@@ -2076,8 +2076,9 @@ if($what=="auto_backup"){
 //Restore Database
 if($what=="database_restore"){
         $filePath =  $_GET['wid'];
-        shell_exec("nohup php restore_database.php ".$filePath.">/dev/null 2>&1 &");
         $info_message = "Database Restore Request Started, This process may take some time complete..." ;
+        shell_exec("nohup php restore_database.php ".$filePath.">/dev/null 2>&1 &");
+        sleep(30);
 }
 
 //Sensor Messages
