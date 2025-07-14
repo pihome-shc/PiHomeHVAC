ca6521e9529066c50ca834b5440da56c53c7602c
twa127 <46624596+twa127@users.noreply.github.com>
2025-07-14 18:09:48 +0100

    Update sensor.php

--- sensor.php
+++ sensor.php
@@ -84,7 +84,7 @@ if (isset($_POST['submit'])) {
 		$error = "<p>".$lang['sensor_record_fail']." </p> <p>" .mysqli_error($conn). "</p>";
 	}
 	$message_success .= "<p>".$lang['do_not_refresh']."</p>";
-	header("Refresh: 10; url=home.php");
+	header("Refresh: 10; url=home.php?page_name=onetouch");
 	// After update on all required tables, set $id to mysqli_insert_id.
 	if ($id==0){$id=$temp_id;}
 }
