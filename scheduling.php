910aedb886b50dd02a92322df36531de457266d8
twa127 <46624596+twa127@users.noreply.github.com>
2025-07-16 19:02:18 +0100

    Update scheduling.php

--- scheduling.php
+++ scheduling.php
@@ -265,7 +265,7 @@ if (isset($_POST['submit'])) {
         $results = $conn->query($query);
         $time_row = mysqli_fetch_assoc($results);
 
-        $query = "SELECT sdt.id as time_id, zone.id as zone_id, sdtz.id as tz_id, zone.name as zone_name, sdtz.status as tz_status, ztype.type, ztype.category, zs.min_c, zs.max_c,
+        $query = "SELECT DISTINCT sdt.id as time_id, zone.id as zone_id, sdtz.id as tz_id, zone.name as zone_name, sdtz.status as tz_status, ztype.type, ztype.category, zs.min_c, zs.max_c,
                 s.sensor_type_id, st.type as stype, sdtz.coop, sdtz.temperature
                 FROM zone
                 JOIN zone_type ztype ON zone.type_id = ztype.id
@@ -278,7 +278,7 @@ if (isset($_POST['submit'])) {
                 ORDER BY zone.index_id asc;";
         $zoneresults = $conn->query($query);
 } else {
-        $query = "SELECT zone.id as tz_id, zone.name as zone_name, zone.status as tz_status, ztype.type, ztype.category, zs.min_c, zs.max_c, s.sensor_type_id, st.type as stype
+        $query = "SELECT DISTINCT zone.id as tz_id, zone.name as zone_name, zone.status as tz_status, ztype.type, ztype.category, zs.min_c, zs.max_c, s.sensor_type_id, st.type as stype
                 FROM zone
                 JOIN zone_type ztype ON zone.type_id = ztype.id
                 LEFT JOIN zone_sensors zs ON zone.id = zs.zone_id
