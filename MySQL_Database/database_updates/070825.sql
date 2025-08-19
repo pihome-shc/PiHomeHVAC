075869bd50f3096f48af23fdd91bdced5ab0d4c3
twa127 <46624596+twa127@users.noreply.github.com>
2025-08-19 08:29:00 +0100

    Update 070825.sql

--- MySQL_Database/database_updates/070825.sql
+++ MySQL_Database/database_updates/070825.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `user` CHANGE IF EXISTS `admin_account` `access_level` TINYINT(4) NULL DEFAULT NULL;
-UPDATE user SET access_level = IF(access_level=0, 2, 1);
-UPDATE user SET access_level = 0 WHERE `username` = 'admin';
+UPDATE user SET `access_level` = IF(`access_level` = 0, 2, `access_level`);
+UPDATE user SET `access_level` = 0 WHERE `username` = 'admin';
+
