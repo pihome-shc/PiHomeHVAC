4985b3f0f01bbb7a3194d5b4294405640db03535
twa127 <46624596+twa127@users.noreply.github.com>
2025-09-10 19:19:15 +0100

    Update gateway.py

--- cron/gateway.py
+++ cron/gateway.py
@@ -26,7 +26,7 @@ print("* MySensors Wifi/Ethernet/Serial Gateway Communication *")
 print("* Script to communicate with MySensors Nodes, for more *")
 print("* info please check MySensors API.                     *")
 print("*      Build Date: 18/09/2017                          *")
-print("*      Version 0.31 - Last Modified 13/03/2025         *")
+print("*      Version 0.31 - Last Modified 10/09/2025         *")
 print("*                                 Have Fun - PiHome.eu *")
 print("********************************************************")
 print(" " + bc.ENDC)
@@ -2397,8 +2397,7 @@ def on_message(client, userdata, message):
                     else:
                         str_attribute = attribute
                 message_str_json = json.loads(message_str)
-                message_str_json = json.loads(message_str)
-                if ("Temperature" in message_str_json and "Humidity" in message_str_json and "DewPoint" in message_str_json and message_str_json['DewPoint'] is not None and mqtt_payload is not None) or ("DewPoint" not in message_str_json and mqtt_payload is not None) :
+                if (isinstance( message_str_json, float) and mqtt_payload is not None) or ("Temperature" in message_str_json and "Humidity" in message_str_json and "DewPoint" in message_str_json and message_str_json['DewPoint'] is not None and mqtt_payload is not None) or ("DewPoint" not in message_str_json and mqtt_payload is not None):
                     # Get reading type (continous or on-change)
                     cur_mqtt.execute(
                         'SELECT sensor_type_id, mode, timeout, correction_factor, resolution FROM sensors WHERE sensor_id = %s AND sensor_child_id = %s LIMIT 1;',
